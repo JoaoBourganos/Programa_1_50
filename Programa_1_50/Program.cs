645e20facd1231cc3d9bea64135ce5b0802c2aa7
joao_ <joao_@PC30K>
2022-06-30 14:29:29 -0300

    Adicionar arquivos de projeto.

---
+++ Programa_1_50/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_1_50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros1, numeros2;
+            for(numeros1 = 0; numeros1 < 51; numeros1++)
+            {
+                Console.WriteLine(numeros1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
